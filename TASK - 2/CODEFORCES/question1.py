8caadb9fd75505c6380ad7bc13d5bf118d7faa20
aryan <56226813+FuryAryan@users.noreply.github.com>
2019-12-28 13:30:01 +0530

    Add files via upload

---
+++ TASK - 2/CODEFORCES/question1.py
@@ -0,0 +1,44 @@
+n , m , a = map(int,input().split())
+
+if n>a and m>a:
+    if n%a!=0:
+        b=n//a+1
+    else:
+        b=n//a
+    if m%a!=0:
+        c=m//a+1
+    else:
+        c=m//a
+    d=b*c
+elif n>a and m<a:
+    if n%a!=0:
+        b=n//a+1
+    else:
+        b=n//a
+    d=b
+elif n<a and m>a:
+    if m%a!=0:
+        c=m//a+1
+    else:
+        c=m//a
+    d=c
+elif m<a and n<a:
+    d=1
+elif m==a and n==a:
+    d=1
+elif n>a and m==a:
+     if n%a!=0:
+        b=n//a+1
+     else:
+         b=n//a
+     d=b
+elif m>a and n==a:
+    if m%a!=0:
+        c=m//a+1
+    else:
+        c=m//a
+    d=c
+else:
+    d=1
+print(d)
+    
