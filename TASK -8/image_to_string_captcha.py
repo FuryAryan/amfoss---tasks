96426657f1387359bb95763f765d90bd13929973
aryan <56226813+FuryAryan@users.noreply.github.com>
2019-12-30 03:07:09 +0530

    Update image_to_string_captcha.py

--- TASK -8/image_to_string_captcha.py
+++ TASK -8/image_to_string_captcha.py
@@ -2,9 +2,9 @@ import PIL import Image #importing python photo library
 import pytesseract as tess #converting image to text
 import cv2 #it is a python library for solving computer vision problem
 
-my_img_cv = cv2.imread("math(1).png") #this is file name
+my_img = cv2.imread("math(1).png") #this is file name
 
-new = tess.image_to_string(my_img_cv) # here image file is converted into string and stored in my_img_cv
+new = tess.image_to_string(my_img) # here image file is converted into string and stored in my_img_cv
 
 my_characters = ['?','=', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
