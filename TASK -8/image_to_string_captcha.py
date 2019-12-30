f20311b033d2c8fc9b2ef46f5bedffab113ee13d
aryan <56226813+FuryAryan@users.noreply.github.com>
2019-12-30 15:11:47 +0530

    Update image_to_string_captcha.py

--- TASK -8/image_to_string_captcha.py
+++ TASK -8/image_to_string_captcha.py
@@ -2,7 +2,7 @@ import PIL import Image #importing python photo library
 import pytesseract as tess #converting image to text
 import cv2 #it is a python library for solving computer vision problem
 
-my_img = cv2.imread("math(1).png") #this is file name
+my_img = cv2.imread("math(1).png") #here it loads the image from specified file
 
 new = tess.image_to_string(my_img) # here image file is converted into string and stored in my_img_cv
 
