8259eac3c77ab48f42c2547d5482279b9059776d
aryan <56226813+FuryAryan@users.noreply.github.com>
2019-12-31 02:21:40 +0530

    Update image_to_string_captcha.py

--- TASK -8/image_to_string_captcha.py
+++ TASK -8/image_to_string_captcha.py
@@ -6,12 +6,6 @@ my_img = cv2.imread("math(1).png") #here it loads the image from specified file
 
 new = tess.image_to_string(my_img) # here image file is converted into string and stored in my_img_cv
 
-my_characters = ['?','=', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-#this is list of the characters
-
-for i in my_characters:
-	new = new.replace(i , '') # now removing unwanted characters in the image file of captcha code
 
 solver = eval(new) # it evaluates the string like python expression and returns the result as integer
 
