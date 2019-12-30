8919a7a273083c3dc68eb00a35aab3ce9948c284
aryan <56226813+FuryAryan@users.noreply.github.com>
2019-12-31 03:08:45 +0530

    Update question3.py

--- TASK - 2/CODEFORCES/question3.py
+++ TASK - 2/CODEFORCES/question3.py
@@ -1,13 +1,13 @@
-n , k = map(int,input().split()) #here taking two inputs as n , k (where k kth place ) and n is number of places
+n , k = map(int,input().split()) 
 
-a = list(map(int,input().split())) # now taking the score here and representing with a
+a = list(map(int,input().split()))
 
-score = a[k] #here a of k means kth place in a
+score = a[k] 
 
-count = 0 #now assuming count be 0
+count = 0
 
-for i in range (len(a)): #and taking acess for every element in the list we use here for loop
-    if(a[i] >= score and a[i] is not 0) : #for qualifying to next round the score must be greater than kth place score or equal to it
-        count = count+1 #it is the total players qualified for next round
+for i in range (len(a)):
+    if(a[i] >= score and a[i] is not 0) : 
+        count = count+1 
 
 print(count)
